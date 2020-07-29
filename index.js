525a0f72656328d6f42e5b85de5b29ffab8a5427
Elena Bialyk <ebialyk@ruckuswireless.com>
2020-07-29 15:42:49 +0300

    SPA control

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
+const path = require('path');
 
 const express = require('express');
 const cors = require('cors');
 
-const { dbConnection } = require('./database/config');
+const {dbConnection} = require('./database/config');
 
 // create the express server
 const app = express();
@@ -21,11 +22,15 @@ dbConnection();
 app.use(express.static('public'));
 
 // routes
-app.use( '/api/users', require('./routes/users'));
-app.use( '/api/login', require('./routes/auth'));
-app.use( '/api/hospitals', require('./routes/hospitals'));
-app.use( '/api/doctors', require('./routes/doctors'));
-app.use( '/api/globalSearch', require('./routes/globalSearch'));
-app.use( '/api/upload', require('./routes/uploads'));
+app.use('/api/users', require('./routes/users'));
+app.use('/api/login', require('./routes/auth'));
+app.use('/api/hospitals', require('./routes/hospitals'));
+app.use('/api/doctors', require('./routes/doctors'));
+app.use('/api/globalSearch', require('./routes/globalSearch'));
+app.use('/api/upload', require('./routes/uploads'));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public/index.html'));
+});
 
 app.listen(process.env.PORT, () => console.log('Port: ', process.env.PORT));
